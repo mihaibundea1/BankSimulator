db1681f302ee3d4db84e48fbe6c0c536af39e392
mihaibundea1 <133668022+mihaibundea1@users.noreply.github.com>
2024-03-09 21:10:05 +0200

    Add files via upload

---
+++ infinityAtomBank-main/InfinityAtomBANK/Card.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace InfinityAtomBANK
+{
+    partial class Card
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblAccount = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblAdmin = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblMounth = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblBalance = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::InfinityAtomBANK.Properties.Resources.Picture8;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(403, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblAccount
+            // 
+            this.lblAccount.Font = new System.Drawing.Font("Cabin", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblAccount.ForeColor = System.Drawing.Color.White;
+            this.lblAccount.Location = new System.Drawing.Point(12, 89);
+            this.lblAccount.Name = "lblAccount";
+            this.lblAccount.Size = new System.Drawing.Size(403, 50);
+            this.lblAccount.TabIndex = 1;
+            this.lblAccount.Text = "[Card Number]";
+            this.lblAccount.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblAccount.Click += new System.EventHandler(this.lblAccount_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cabin", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name:";
+            // 
+            // lblAdmin
+            // 
+            this.lblAdmin.Font = new System.Drawing.Font("Cabin", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblAdmin.ForeColor = System.Drawing.Color.White;
+            this.lblAdmin.Location = new System.Drawing.Point(151, 244);
+            this.lblAdmin.Name = "lblAdmin";
+            this.lblAdmin.Size = new System.Drawing.Size(264, 19);
+            this.lblAdmin.TabIndex = 3;
+            this.lblAdmin.Text = "[isAdmin]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cabin", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Expiration Date:";
+            // 
+            // lblMounth
+            // 
+            this.lblMounth.AutoSize = true;
+            this.lblMounth.Font = new System.Drawing.Font("Cabin", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblMounth.ForeColor = System.Drawing.Color.White;
+            this.lblMounth.Location = new System.Drawing.Point(113, 178);
+            this.lblMounth.Name = "lblMounth";
+            this.lblMounth.Size = new System.Drawing.Size(60, 19);
+            this.lblMounth.TabIndex = 5;
+            this.lblMounth.Text = "[Mounth]";
+            this.lblMounth.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cabin", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(179, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 19);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "/";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Font = new System.Drawing.Font("Cabin", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblYear.ForeColor = System.Drawing.Color.White;
+            this.lblYear.Location = new System.Drawing.Point(199, 178);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(41, 19);
+            this.lblYear.TabIndex = 7;
+            this.lblYear.Text = "[Year]";
+            this.lblYear.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Cabin", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(12, 210);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 19);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Balance:";
+            // 
+            // lblBalance
+            // 
+            this.lblBalance.Font = new System.Drawing.Font("Cabin", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBalance.ForeColor = System.Drawing.Color.White;
+            this.lblBalance.Location = new System.Drawing.Point(73, 210);
+            this.lblBalance.Name = "lblBalance";
+            this.lblBalance.Size = new System.Drawing.Size(342, 19);
+            this.lblBalance.TabIndex = 9;
+            this.lblBalance.Text = "[Balance]";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Cabin", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(12, 244);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(133, 19);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Administrator Account:";
+            // 
+            // lblName
+            // 
+            this.lblName.Font = new System.Drawing.Font("Cabin", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(63, 149);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(352, 19);
+            this.lblName.TabIndex = 11;
+            this.lblName.Text = "[Name]";
+            // 
+            // Card
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(427, 272);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblBalance);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblMounth);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblAdmin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblAccount);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Card";
+            this.Text = "Card";
+            this.Load += new System.EventHandler(this.Card_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblAccount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblAdmin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblMounth;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblBalance;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblName;
+    }
+}
---
+++ infinityAtomBank-main/README.md
@@ -0,0 +1,5 @@
+# infinityAtomBank
+
+ATENȚIE PENTRU CA PROGRAMUL SĂ FUNCȚIONEZE,la adresa
+AppData\Roaming (Mai simplu: %appdata%)\AtomB (Acest folder trebuie creat) (Adresa completa: %appdata%\AtomB), atașați fișierul 0000.txt
+Și conectați-vă cu contul 0000 și pin-ul 1234
